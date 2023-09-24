4341aa700d2959a3b80293b205e34d814bf77422
xeuxdev <headbwoi25@gmail.com>
2023-09-24 19:22:15 +0100

    fix base url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import { siteConfig } from "@/config/site"
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -5,6 +6,9 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const BASE_URL = process.env.BASE_URL
-  ? process.env.BASE_URL
-  : "http://localhost:3000"
+export const BASE_URL =
+  process.env.BASE_URL == undefined
+    ? process.env.NODE_ENV === "production"
+      ? `${siteConfig.url}`
+      : "http://localhost:3000"
+    : process.env.BASE_URL
